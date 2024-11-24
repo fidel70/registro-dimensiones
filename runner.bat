7db054f564e694753a71574caa0ac8d46fa78167
fidel70 <fid.l.mori70@gmail.com>
2024-11-23 20:32:46 -0500

    Primer commit

---
+++ runner.bat
@@ -0,0 +1,7 @@
+echo "# dimensiones" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/fidel70/dimensiones.git
+git push -u origin main
